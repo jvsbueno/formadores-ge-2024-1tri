55561924e3c94fab689afb65cec5ec3e9753af42
João Víctor Santos Bueno <104871687+jvsbueno@users.noreply.github.com>
2024-03-19 19:44:51 +0000

    oi

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-
 for(let i=0; i<botoes.length;i++){
     botoes[i].onclick = function(){
         for(let j=0;j<botoes.length;j++){
@@ -11,4 +10,33 @@ for(let i=0; i<botoes.length;i++){
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+}
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00");
+const tempoObjetivo2 = new Date("2024-12-31T00:00");
+const tempoObjetivo3 = new Date("2024-08-01T00:00");
+const tempoObjetivo4 = new Date("2024-04-30T00:00");
+
+
+contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+contadores[2].textContent = calculaTempo(tempoObjetivo3);
+contadores[3].textContent = calculaTempo(tempoObjetivo4);
+
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60; 
+    minutos %= 60;
+    horas %= 60;
+
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+
 }
