cacd6684c2581e1e750ff8a09aea2a015f08bc0a
João Víctor Santos Bueno <104871687+jvsbueno@users.noreply.github.com>
2024-04-16 20:07:43 +0000

    oi

--- main.js
+++ main.js
@@ -14,10 +14,15 @@ for(let i=0; i<botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-31T00:00:00");
-const tempoObjetivo3 = new Date("2024-08-01T00:00:00");
-const tempoObjetivo4 = new Date("2024-04-30T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-12-31T00:00:00");
+const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
 
+const tempos = {tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4};
+
+for(let i=0; i<contadores.length; i++){
+    contadores[i].textContent = calculaTempo(tempos[i]);
+}
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 contadores[1].textContent = calculaTempo(tempoObjetivo2);
